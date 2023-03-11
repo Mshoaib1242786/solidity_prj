132ef66da08507a9586abc2e04fcb48aff17bc9c
MUHAMMAD SHOAIB <108264286+Mshoaib1242786@users.noreply.github.com>
2023-03-11 01:08:49 -1200

    solidity

---
+++ Block.js
@@ -0,0 +1,64 @@
+
+const hexToBinary = require("hex-to-binary");
+const {GENESIS_DATA, MINE_RATE}= require('./config');
+const cryptoHash = require("./crypto-hash")
+class Block{
+    constructor({timestamp, prevHash,hash,data, nonce, difficulty}){
+      this.timestamp = timestamp;
+      this.prevHash=prevHash;
+      this.hash=hash;
+      this.data=data;
+      this.nonce =nonce;
+      this.difficulty=difficulty;
+    }
+    static genesis(){
+        return new this (GENESIS_DATA);
+    }
+    static mineBlock({prevBlock,data}){
+       let hash,timestamp;
+        const prevHash = prevBlock.hash;
+        // const {difficulty} = prevBlock;
+        let {difficulty} =prevBlock;
+
+
+        let nonce=0;
+        do{
+            nonce++;
+            timestamp=Date.now();//00cdef, 00
+            difficulty=Block.adjustDifficulty({
+                originalBlock: prevBlock,
+                timestamp})
+            hash= cryptoHash(timestamp,prevHash,data,nonce,difficulty);
+        }while(hexToBinary(hash).substring(0,difficulty) !=='0'.repeat(difficulty));
+        return new this ({
+            timestamp,
+            prevHash,
+            data,
+            difficulty,
+            nonce, 
+            hash,
+        });
+    }
+    static adjustDifficulty({originalBlock,timestamp}){
+      const {difficulty}= originalBlock;
+      if(difficulty<1) return 1;
+      const difference =timestamp-originalBlock.timestamp;
+      if(difference>MINE_RATE) return difficulty-1;
+      return difficulty+1;
+    
+    }
+}
+const block1 = new Block({
+    hash:'0xabc',
+    timestamp:'20/1/2023',
+    prevHash:'0x123',
+    data:'hello'
+});
+
+// const genesisBlock = Block.genesis();
+// console.log(genesisBlock);
+// const result = Block.mineBlock({prevBlock:block1,data:"block2"});
+// console.log(result);
+// Genesis block
+module.exports= Block;
+
